80e6c8931d96e39715ab7917aea1bdc0fd84208e
RENZOZ78 <didierbap@gmail.com>
2022-03-07 16:20:15 +0100

    templates+admin

---
+++ src/main/java/Ecommerce/MagicShopApplication.java
@@ -0,0 +1,13 @@
+package Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicShopApplication.class, args);
+	}
+
+}
